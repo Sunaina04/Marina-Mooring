4299ef416404645ebe895e63ec60987bec235259
Sunaina <sunaina.takhi@genboot.com>
2024-03-18 14:13:40 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginForm from './Components/LoginForm';
+import Home from './Pages/Home';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <>
-    <Routes>
-      <Route path='/' element={<LoginForm/>}/>
-    </Routes>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginForm />} />
+        {/* Add more routes here as needed */}
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
