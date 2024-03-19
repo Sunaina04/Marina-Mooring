780ebb654f6fa293aa7f79fd43851ce6d90c794f
priyasingla17 <priya.singla@genboot.com>
2024-03-19 16:52:54 +0530

    signUPCSSChanges

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,20 +1,77 @@
+import "./SignUp.css";
+import { FaFacebook } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 const SignUp=()=>{
     const changeHandle=(e)=>{}
-    return <div>
-        <form className="signUpForm">
-            <div className="container">
-            <h2 className="signup">SignUp</h2>
-            <input type="text" placeholder="Email" name="email" required onChange={changeHandle}/>
-            <input type="password" placeholder="Create password" name="pwd" required/>
-            <input type="password" placeholder="Confirm password" name="pwd_confirm" required/>
-            <input type="submit" value="Signup" className="signUpBtn"/>
+    return <div className="main-container">
         
-        <h6 style={{width:"50%",textAlign:"center"}}>Already have an account?<a href="#login">Login</a></h6>
-        <h6 style={{width:"50%",textAlign:"center"}}>------or------</h6>
-        <input type="button" value="Login with facebook" className="facebookBtn"/><br/>
-        <input type="button" value="Login with Google" className="googleBtn"/> 
+            <div className="container">
+                <div className="signUp_Form">
+            <h1>SignUp</h1>
+            <div className="input">
+                <div className="email">
+                <input type="text" placeholder="Email" name="email" required onChange={changeHandle}/>
+            
+                </div>
+                <div className="password">
+                <input type="password" placeholder="Create password" name="pwd" required/>
+
+                </div>
+                <div className="confirmPassword">
+                <input type="password" placeholder="Confirm password" name="pwd_confirm" required/>
+                </div>
+            </div>
+            <div className="signUp-btn">
+                            <button>SignUp</button>
+
+                            <p style={{ fontSize: "0.90rem" }}>Already have an account? <span style={{ color: "blue", cursor:"pointer" }}>Login</span></p>
+                        </div>
+                        <div className="other">
+                            <hr />
+
+                        </div>
+        
+                        <div className="btn-Container">
+                            <div className="btn-facebook">
+                                <button>
+
+                                    <div className="iconFacebook">
+
+                                        <FaFacebook fontSize={20} />
+
+                                        <span style={{ marginLeft: "4rem", }}>
+                                            Login with Facebook
+                                        </span>
+
+                                    </div>
+
+                                </button>
+                            </div>
+                            <div className="btn-google">
+                                <button>
+
+                                    <div className="iconGoogle">
+
+                                        <FcGoogle fontSize={20} />
+
+                                        <span style={{ marginLeft: "4rem" }}>
+                                            Login with Google
+                                        </span>
+
+
+                                    </div>
+
+
+
+                                </button>
+
+
+                            </div>
+                        </div>
+
         </div>
-        </form> 
+        </div>
+            
 
     </div>
 }
