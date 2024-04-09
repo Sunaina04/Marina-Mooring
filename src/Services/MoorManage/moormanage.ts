991bfec4da43a7b1cf999a03af4123de5705a844
Sunaina04 <sunaina.takhi@genboot.com>
2024-04-09 14:59:29 +0530

    Moorings API

--- src/Services/MoorManage/moormanage.ts
+++ src/Services/MoorManage/moormanage.ts
@@ -1,5 +1,5 @@
 import { userApi } from "../userApi";
-import { CUSTOMER_PAYLOAD } from "./types";
+import { CUSTOMER_PAYLOAD, MOORING_PAYLOAD } from "./types";
 
 const moormanageApi = userApi.injectEndpoints({
   endpoints: (builder: any) => ({
@@ -20,7 +20,7 @@ const moormanageApi = userApi.injectEndpoints({
 
     deleteCustomer: builder.mutation({
       query: ({ id }: { id?: number }) => ({
-        url: `api/v1/customer/${id}`, // Append the id to the URL
+        url: `api/v1/customer/${id}`,
         method: "DELETE",
       }),
     }),
@@ -28,6 +28,54 @@ const moormanageApi = userApi.injectEndpoints({
     updateCustomer: builder.mutation({
       query: (payload: CUSTOMER_PAYLOAD) => ({
         url: "api/v1/customer/",
+        method: "PUT",
+        body: payload,
+      }),
+    }),
+
+    addMoorings: builder.mutation({
+      query: (payload: MOORING_PAYLOAD) => ({
+        url: "api/v1/mooring/",
+        method: "POST",
+        body: payload,
+      }),
+    }),
+
+    getMoorings: builder.mutation({
+      query: ({
+        page,
+        size,
+        sortBy,
+        sortDir,
+      }: {
+        page?: number;
+        size?: number;
+        sortBy?: string;
+        sortDir?: string;
+      }) => ({
+        url: "api/v1/mooring/",
+        method: "GET",
+        params: { page, size, sortBy, sortDir },
+      }),
+    }),
+
+    // getMoorings: builder.mutation({
+    //   query: ({}) => ({
+    //     url: "api/v1/mooring/",
+    //     method: "GET",
+    //   }),
+    // }),
+
+    deleteMoorings: builder.mutation({
+      query: ({ id }: { id?: number }) => ({
+        url: `api/v1/mooring/${id}`,
+        method: "DELETE",
+      }),
+    }),
+
+    updateMoorings: builder.mutation({
+      query: ({ payload, id }: { payload: CUSTOMER_PAYLOAD; id: number }) => ({
+        url: `api/v1/mooring/${id}`,
         method: "PUT",
         body: payload,
       }),
@@ -40,4 +88,8 @@ export const {
   useAddCustomerMutation,
   useDeleteCustomerMutation,
   useUpdateCustomerMutation,
+  useAddMooringsMutation,
+  useGetMooringsMutation,
+  useDeleteMooringsMutation,
+  useUpdateMooringsMutation,
 } = moormanageApi;
