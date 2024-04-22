661fbeebc8d2f77bc8264cff4b1436ebfb5c9f8a
Sunaina04 <sunaina.takhi@genboot.com>
2024-04-22 14:44:59 +0530

    WorkOrder API

--- src/Components/Moorserve/WorkOrders/workOrders.tsx
+++ src/Components/Moorserve/WorkOrders/workOrders.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import ButtonComponent from "../../Common/ButtonComponent";
@@ -8,6 +8,11 @@ import { InputText } from "primereact/inputtext";
 import { Dialog } from "primereact/dialog";
 import { Button } from "primereact/button";
 import AddWorkOrders from "./AddWorkOrders";
+import {
+  WorkOrder_PAYLOAD,
+  WorkOrder_RESPONSE,
+} from "../../../Services/MoorServe/types";
+import { useGetWorkOrdersMutation } from "../../../Services/MoorServe/moorserve";
 
 interface CustomerData {
   id: string;
@@ -20,50 +25,18 @@ interface CustomerData {
 
 const WorkOrders = () => {
   const [visible, setVisible] = useState(false);
-  const [boatData, setBoatData] = useState<CustomerData[]>([
-    {
-      id: "01",
-      boatName: "Suncatcher",
-      name: "John Smith",
-      date: "15, March 2024 to 15, March 2024",
-      measurement: "Length: 10m, Width: 3.8m",
-      place: "Boatyard",
-    },
-    {
-      id: "01",
-      boatName: "Suncatcher",
-      name: "John Smith",
-      date: "15, March 2024 to 15, March 2024",
-      measurement: "Length: 10m, Width: 3.8m",
-      place: "Boatyard",
-    },
-    {
-      id: "01",
-      boatName: "Suncatcher",
-      name: "John Smith",
-      date: "15, March 2024 to 15, March 2024",
-      measurement: "Length: 10m, Width: 3.8m",
-      place: "Boatyard",
-    },
-    {
-      id: "01",
-      boatName: "Suncatcher",
-      name: "John Smith",
-      date: "15, March 2024 to 15, March 2024",
-      measurement: "Length: 10m, Width: 3.8m",
-      place: "Boatyard",
-    },
-  ]);
-
-
-
+  const [workOrderData, setWorkOrderData] = useState<WorkOrder_PAYLOAD[]>([]);
+  const [getWorkOrder] = useGetWorkOrdersMutation();
 
   const header = (
     <div className="flex flex-wrap align-items-center justify-between  p-4">
       <span className="text-xl font-bold">Work Orders</span>
       <div className="">
         <div className="p-input-icon-left">
-          <i className="pi pi-search text-[#D2D2D2] " data-testid="search-icon" />
+          <i
+            className="pi pi-search text-[#D2D2D2] "
+            data-testid="search-icon"
+          />
           <InputText
             placeholder="Search"
             className="h-[5vh] cursor-pointer font-bold"
@@ -72,6 +45,25 @@ const WorkOrders = () => {
       </div>
     </div>
   );
+
+  const getWorkOrderData = async () => {
+    await getWorkOrder({})
+      .unwrap()
+      .then(async (response: any) => {
+        console.log("RESPONSE", response);
+        const { status, content } = response as WorkOrder_RESPONSE;
+        if (status === 200 && Array.isArray(content)) {
+          setWorkOrderData(content);
+          // setFilteredMooringData(content); // Initialize filtered data with all data
+        }
+      });
+  };
+
+  const handleEdit = (rowData: any) => {};
+
+  useEffect(() => {
+    getWorkOrderData();
+  }, []);
 
   return (
     <>
@@ -84,8 +76,6 @@ const WorkOrders = () => {
           </div>
           <div className="flex mr-36 gap-4">
             <div>
-
-
               <div className="flex gap-4">
                 <Button
                   label={"Create New"}
@@ -99,18 +89,15 @@ const WorkOrders = () => {
                     fontWeight: "bold",
                     fontSize: "0.80vw",
                   }}
-                >
-                </Button>
+                ></Button>
 
                 <Dialog
                   header={""}
                   visible={visible}
                   modal={false}
-
-                  style={{ width: '50vw' }} onHide={() => setVisible(false)}
-
+                  style={{ width: "50vw" }}
+                  onHide={() => setVisible(false)}
                 >
-
                   <AddWorkOrders />
                 </Dialog>
               </div>
@@ -120,7 +107,7 @@ const WorkOrders = () => {
 
         <div className="bg-[#F2F2F2] rounded-xl border-[1px] border-[#D1D1D1] ml-36 p-2 mt-12 w-[64vw] ">
           <DataTable
-            value={boatData}
+            value={workOrderData}
             header={header}
             tableStyle={{
               // width: "73rem",
@@ -129,41 +116,46 @@ const WorkOrders = () => {
             }}
             scrollable={true}
           >
-            <Column style={{ width: "4vw" }} field="id" header="ID"></Column>
+            <Column
+              style={{ width: "4vw" }}
+              field="id"
+              header="Customer ID"
+            ></Column>
             <Column
               style={{ width: "7vw" }}
-              field="boatName"
+              field="customerName"
               header="Customer Name"
             ></Column>
             <Column
               style={{ width: "7vw" }}
-              field="name"
-              header="Mooring Number"
+              field="mooringNumber"
+              header="Mooring ID"
             ></Column>
             <Column
               style={{ width: "15vw" }}
-              field="date"
+              field="boatYard"
               header="Boatyard"
             ></Column>
             <Column
               style={{ width: "13vw" }}
-              field="measurement"
+              field="assignedTo"
               header="Assigned to"
             ></Column>
             <Column
               style={{ width: "6vw" }}
-              field="place"
+              field="dueDate"
               header="Due date"
+            ></Column>
+            <Column
+              style={{ width: "6vw" }}
+              field="status"
+              header="Status"
             ></Column>
             <Column
               header="Action"
               body={() => (
                 <div className="flex gap-4">
-                  <span className="text-green  cursor-pointer">
-                    New Request
-                  </span>
-
-                  <span className="text-green underline cursor-pointer">
+                  <span className="text-black underline cursor-pointer">
                     Edit
                   </span>
                 </div>
--- src/Services/MoorServe/types.ts
+++ src/Services/MoorServe/types.ts
@@ -25,3 +25,43 @@ export interface UPLOAD_PAYLOAD {
   customerName: string;
   customerId: string;
 }
+
+export interface WorkOrder_PAYLOAD {
+  id: number;
+  creationDate: string;
+  createdBy: string;
+  lastModifiedDate: string;
+  lastModifiedBy: string;
+  customerName: string;
+  mooringNumber: string;
+  boatYard: string;
+  assignedTo: string;
+  dueDate: string;
+  scheduleDate: string;
+  status: string;
+  time: string;
+  reportProblem: string;
+}
+
+export interface WorkOrder_RESPONSE {
+  message: string;
+  status: number;
+  errorList: [];
+  time: string;
+  content: {
+    id: number;
+    creationDate: string;
+    createdBy: string;
+    lastModifiedDate: string;
+    lastModifiedBy: string;
+    customerName: string;
+    mooringNumber: string;
+    boatYard: string;
+    assignedTo: string;
+    dueDate: string;
+    scheduleDate: string;
+    status: string;
+    time: string;
+    reportProblem: string;
+  };
+}
