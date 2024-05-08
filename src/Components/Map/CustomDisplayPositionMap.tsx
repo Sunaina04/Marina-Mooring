90d627dce0ebf8567f8cbe865f2426a94d061c45
Sunaina04 <sunaina.takhi@genboot.com>
2024-05-08 12:45:21 +0530

    chnges

--- src/Components/Map/CustomDisplayPositionMap.tsx
+++ src/Components/Map/CustomDisplayPositionMap.tsx
@@ -2,6 +2,7 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import L from 'leaflet'
 import './CustomMap.css'
 import { CustomMapProps } from '../../Type/ComponentBasedType'
+import DefaultIcon from './DefaultIcon'
 
 const CustomDisplayPositionMap: React.FC<CustomMapProps> = ({
   position = [31.63398, 74.872261],
@@ -18,12 +19,6 @@ const CustomDisplayPositionMap: React.FC<CustomMapProps> = ({
   )
 }
 
-let DefaultIcon = L.icon({
-  iconUrl: '/assets/images/marker-icon.png',
-  iconSize: [100, 100],
-  iconAnchor: [10, 41],
-  popupAnchor: [2, -40],
-})
 L.Marker.prototype.options.icon = DefaultIcon
 
 export default CustomDisplayPositionMap
