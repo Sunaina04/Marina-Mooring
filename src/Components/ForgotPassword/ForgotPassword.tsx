cb94d7776912a9e91c2b820f1e1a53ce5b2f8732
ArunGenboot <arun.kumar@genboot.com>
2024-05-09 15:43:51 +0530

    worked on login Page

--- src/Components/ForgotPassword/ForgotPassword.tsx
+++ src/Components/ForgotPassword/ForgotPassword.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import InputComponent from '../CommonComponent/InputComponent'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { ErrorResponseForgotPassword, validateEmailResponse } from '../../Type/ApiTypes'
 import { Button } from 'primereact/button'
 import { validateUserCredentials } from '../Utils/ValidateUserCredentials'
 import { useForgotPasswordMutation } from '../../Services/Authentication/AuthApi'
+import { InputText } from 'primereact/inputtext'
 
 const ForgotPassword = () => {
   const navigate = useNavigate()
@@ -52,7 +53,7 @@ const ForgotPassword = () => {
 
   return (
     <>
-      <div className="w-full h-screen flex justify-center items-center">
+      {/* <div className="w-full h-screen flex justify-center items-center">
         <div className="text-center">
           <div className="mb-4">
             <div>
@@ -121,7 +122,114 @@ const ForgotPassword = () => {
             <span className="w-8 h-[0.50px] bg-black text-blue-700 flex justify-center"></span>
           </div>
         </div>
+      </div> */}
+
+
+
+      <div
+        className="w-full h-screen flex justify-center items-center"
+        style={{
+          backgroundImage: "url('/assets/images/loginBackgroundImage.png')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}>
+
+        <div className="bg-white rounded-xl p-8 w-600 absolute top-227 left-420 gap-8 h-auto">
+          <div className="text-center mt-[1rem]">
+            <img
+              src="/assets/images/moorfindLogo.png"
+              alt="Logo"
+              className="mx-auto w-60 h-14 mb-5 "
+            />
+          </div>
+
+          <div className=" flex flex-col justify-center text-center mt-[3rem]">
+            <div className="text-red-500 ">{errors.email}</div>
+            <div className='flex flex-col gap-5'>
+
+              <div className="p-input-icon-left relative flex justify-center ">
+                <InputText
+                  style={{
+                    width: '28vw',
+                    height: '7vh',
+                    padding: '0 4rem 0 3rem',
+                    border: '1px solid #C5D9E0',
+                    fontSize: '16px',
+                    color: '#00426F',
+                    borderRadius: '10px',
+                  }}
+
+                  placeholder={"Enter your register email"}
+                  name="username"
+                  value={""}
+                  onChange={handleChange}
+                />
+                <span
+                  className="w-5 h-5 absolute top-[2rem]  transform -translate-y-1/2 left-4 text-[#00426f] text-lg font-[900]"
+                  style={{
+                    backgroundImage: `url('/assets/images/key.png' : '/assets/images/envelope.png'
+                      })`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'contain',
+                  }}>
+
+                </span>
+
+
+              </div>
+            </div>
+
+
+            <div className="flex  mt-8 cursor-pointer ">
+              <p className="w-[42vw] text-xs font-bold">
+                If you are having trouble logging in, please enter the email address registered with
+                MOORFIND. If it is a valid email address, you will be sent an email allowing you to
+                resest your password.
+              </p>
+            </div>
+
+
+          </div>
+
+
+
+          <div className="flex flex-col items-center mt-8">
+            <Button
+              style={{
+                width: '28vw',
+                height: '7vh',
+                padding: '0 4rem 0 3rem',
+                border: '1px solid #C5D9E0',
+                fontSize: '22px',
+                lineHeight: "25.78px",
+                color: '#FFFFFF',
+                borderRadius: '10px',
+                backgroundColor: "#0098FF",
+                textAlign: "center",
+                display: "flex",
+                fontWeight: "700",
+                justifyContent: "center",
+              }}
+            >
+
+
+              Submit
+            </Button>
+
+            <p className='mt-10 text-[#00426F] font-[700] text-lg'>Back</p>
+
+
+
+
+          </div>
+        </div>
+
       </div>
+
+
+
+
+
     </>
   )
 }
--- src/Components/Login/LoginForm.tsx
+++ src/Components/Login/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import {
   useGetEmployeeMutation,
   useLoginMutation,
@@ -11,6 +11,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import { InputText } from 'primereact/inputtext'
 import { LoginFormProps } from '../../Type/ComponentBasedType'
 import { Button } from 'primereact/button'
+import './Login.module.css'
 
 export default function LoginForm({
   Label,
@@ -146,90 +147,96 @@ export default function LoginForm({
           backgroundSize: 'cover',
           backgroundPosition: 'center',
         }}>
-        <div className="bg-white rounded-lg p-8 w-600 h-700 absolute top-227 left-420 gap-8">
-          <div className="text-center mb-3">
+        <div
+          className={`bg-white rounded-xl p-8 w-600 absolute top-227 left-420 gap-8 ${showSinUp ? 'h-[70vh]' : 'h-auto'}`}>
+          <div className="text-center mt-[1rem]">
             <img
               src="/assets/images/moorfindLogo.png"
               alt="Logo"
-              className="mx-auto w-15 h-12 mb-5"
+              className="mx-auto w-60 h-14 mb-5 "
             />
-            <div className="text-red-500">{errors.email}</div>
-            <div className="p-input-icon-left relative">
-              <InputText
-                style={{
-                  width: '30vw',
-                  height: '6vh',
-                  padding: '0 4rem 0 3rem',
-                  border: '1px solid #C5D9E0',
-                  fontSize: '16px',
-                  color: '#00426F',
-                  borderRadius: '10px',
-                }}
-                type={
-                  showSinUp
-                    ? typeEmail === 'password'
-                      ? 'password'
-                      : 'text'
-                    : typeEmail === 'email'
-                      ? 'email'
-                      : 'text'
-                }
-                placeholder={showSinUp ? 'New password' : 'Enter Your email'}
-                name="username"
-                value={username}
-                onChange={handleChange}
-              />
-              <span
-                className="w-5 h-5 absolute top-1/2 transform -translate-y-1/2 left-3 text-gray-400"
-                style={{
-                  backgroundImage: `url(${
-                    showSinUp ? '/assets/images/key.png' : '/assets/images/envelope.png'
-                  })`,
-                  backgroundRepeat: 'no-repeat',
-                  backgroundSize: 'contain',
-                }}></span>
-            </div>
           </div>
-          <div className="mb-3">
-            <div className="p-input-icon-left relative">
-              <InputText
-                style={{
-                  width: '30vw',
-                  height: '6vh',
-                  padding: '0 4rem 0 3rem',
-                  border: '1px solid #C5D9E0',
-                  fontSize: '16px',
-                  color: '#00426F',
-                  borderRadius: '10px',
-                }}
-                type={
-                  showSinUp
-                    ? typePass === 'password'
-                      ? 'password'
-                      : 'text'
-                    : typePass === 'password'
-                      ? 'password'
-                      : 'text'
-                }
-                placeholder={showSinUp ? 'Confirm password' : 'Password'}
-                name="password"
-                value={password}
-                onChange={handleChange}
-              />
-              <span
-                className="w-5 h-5 absolute top-1/2 transform -translate-y-1/2 left-3 text-gray-400"
-                style={{
-                  backgroundImage: `url(${
-                    admin ? '/assets/images/key.png' : '/assets/images/key.png'
-                  })`,
-                  backgroundRepeat: 'no-repeat',
-                  backgroundSize: 'contain',
-                }}></span>
+
+          <div
+            className={` flex flex-col justify-center text-center ${showSinUp ? 'mt-[6rem]' : 'mt-[3rem]'}`}>
+            <div className="text-red-500 ">{errors.email}</div>
+            <div className="flex flex-col gap-5">
+              <div className="p-input-icon-left relative flex justify-center ">
+                <InputText
+                  style={{
+                    width: '28vw',
+                    height: '7vh',
+                    padding: '0 4rem 0 3rem',
+                    border: '1px solid #C5D9E0',
+                    fontSize: '16px',
+                    color: '#00426F',
+                    borderRadius: '10px',
+                  }}
+                  type={
+                    showSinUp
+                      ? typeEmail === 'password'
+                        ? 'password'
+                        : 'text'
+                      : typeEmail === 'email'
+                        ? 'email'
+                        : 'text'
+                  }
+                  placeholder={showSinUp ? 'New password' : 'johnsmith@sub-admin.com'}
+                  name="username"
+                  value={username}
+                  onChange={handleChange}
+                />
+                <span
+                  className="w-5 h-5 absolute top-[2rem]  transform -translate-y-1/2 left-4 text-[#00426f] text-lg font-[900]"
+                  style={{
+                    backgroundImage: `url(${
+                      showSinUp ? '/assets/images/key.png' : '/assets/images/envelope.png'
+                    })`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'contain',
+                  }}></span>
+              </div>
+
+              <div className="p-input-icon-left relative">
+                <InputText
+                  style={{
+                    width: '28vw',
+                    height: '7vh',
+                    padding: '0 4rem 0 3rem',
+                    border: '1px solid #C5D9E0',
+                    fontSize: '16px',
+                    color: '#00426F',
+                    borderRadius: '10px',
+                  }}
+                  type={
+                    showSinUp
+                      ? typePass === 'password'
+                        ? 'password'
+                        : 'text'
+                      : typePass === 'password'
+                        ? 'password'
+                        : 'text'
+                  }
+                  placeholder={showSinUp ? 'Confirm password' : '**********'}
+                  name="password"
+                  value={password}
+                  onChange={handleChange}
+                />
+                <span
+                  className="w-5 h-5 absolute top-1/2 transform -translate-y-1/2 left-4 text-gray-400"
+                  style={{
+                    backgroundImage: `url(${
+                      admin ? '/assets/images/key.png' : '/assets/images/key.png'
+                    })`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'contain',
+                  }}></span>
+              </div>
             </div>
 
             {!showSinUp && !admin && (
               <>
-                <div className="flex justify-end mt-8 cursor-pointer ">
+                <div className="flex justify-end mt-8 cursor-pointer underline">
                   <Link to={'/forgotPassword'}>
                     <p
                       className="font-normal"
@@ -244,41 +251,51 @@ export default function LoginForm({
                     </p>
                   </Link>
                 </div>
-
-                <div className="flex justify-end">
-                  <span className="w-[7.40rem] h-[0.50px] bg-black text-black "></span>
-                </div>
               </>
             )}
           </div>
-          <div className="flex flex-col items-center">
+
+          <div className="flex flex-col items-center mt-8">
             {admin && <span className="mb-8">For admin use only</span>}{' '}
             <Button
-              className="w-30 h-10 bg-black text-white border border-black font-bold text-sm flex justify-center items-center"
-              style={{ backgroundColor: '#0098FF', width: '30vw' }}
+              style={{
+                width: '28vw',
+                height: '7vh',
+                padding: '0 4rem 0 3rem',
+                border: '1px solid #C5D9E0',
+                fontSize: '22px',
+                lineHeight: '25.78px',
+                color: '#FFFFFF',
+                borderRadius: '10px',
+                backgroundColor: '#0098FF',
+                textAlign: 'center',
+                display: 'flex',
+                fontWeight: '700',
+                justifyContent: 'center',
+              }}
               onClick={showSinUp ? ResetPasswordHandler : signInHandler}>
-              {Label}
+              {showSinUp ? 'Confirm' : Label}
+
+              {/* {Label} */}
             </Button>
-            <div className="flex justify-center items-center mt-0">
-              <div
-                className="text-center mx-auto"
-                style={{
-                  width: '30vw',
-                  fontSize: '14px',
-                  lineHeight: '24px',
-                  textAlign: 'center',
-                  color: '#00426F',
-                }}>
-                <p className="text-xs font-bold">
-                  Just testing the waters? If you do not have an account{' '}
-                  <a href="https://www.moorfind.com/" className="underline font-bolder">
-                    CLICK HERE
-                  </a>{' '}
-                  to let us know you would like to connect and see if MOORFIND can work for you and
-                  your business.
-                </p>
+            {showSinUp ? (
+              <p className="mt-10 text-[#00426F] font-[700] text-lg">Back</p>
+            ) : (
+              <div className="flex justify-center items-center mt-8">
+                <div>
+                  <p className="text-sm w-[28vw] text-center mt-2 text-[#00426F]  leading-6 font-[400] font">
+                    Just testing the waters? If you do not have an account
+                    <a
+                      href="https://www.moorfind.com/"
+                      className="underline font-bolder font-[700] text-sm">
+                      CLICK HERE
+                    </a>
+                    to let us <br /> know you would like to connect and see if MOORFIND can work for
+                    you and your business.
+                  </p>
+                </div>
               </div>
-            </div>
+            )}
           </div>
         </div>
       </div>
