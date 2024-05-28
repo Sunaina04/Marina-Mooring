f72a8c9c1d96fd57d3dd0d219fb7d4dd0e68ba64
Sunaina04 <sunaina.takhi@genboot.com>
2024-05-28 17:03:02 +0530

    ui changes

--- src/Components/Map/DefaultIcon.ts
+++ src/Components/Map/DefaultIcon.ts
@@ -1,12 +1,12 @@
-import L, { IconOptions } from 'leaflet';
+import L, { IconOptions } from 'leaflet'
 
 const defaultIconOptions: IconOptions = {
   iconUrl: '/assets/images/marker-icon.png',
-  iconSize: [100, 100],
+  iconSize: [200, 200],
   iconAnchor: [10, 41],
   popupAnchor: [2, -40],
-};
+}
 
-const DefaultIcon = L.icon(defaultIconOptions);
+const DefaultIcon = L.icon(defaultIconOptions)
 
-export default DefaultIcon;
+export default DefaultIcon
