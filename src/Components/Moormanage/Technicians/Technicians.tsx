46586e3dc63eb171cbecfe008aeff10d683811f8
ArunGenboot <arun.kumar@genboot.com>
2024-06-14 14:09:27 +0530

    worked on technician DataTable

--- src/Components/Moormanage/Technicians/Technicians.tsx
+++ src/Components/Moormanage/Technicians/Technicians.tsx
@@ -14,6 +14,8 @@ import Header from '../../Layout/LayoutComponents/Header'
 import DataTableComponent from '../../CommonComponent/Table/DataTableComponent'
 import InputTextWithHeader from '../../CommonComponent/Table/InputTextWithHeader'
 import { properties } from '../../Utils/MeassageProperties'
+import { ProgressSpinner } from 'primereact/progressspinner'
+import { ActionButtonColumnProps } from '../../../Type/Components/TableTypes'
 
 const useFetchTechnicians = () => {
   const [technicianData, setTechnicianData] = useState<TechnicianPayload[]>([])
@@ -49,7 +51,7 @@ const Technicians = () => {
   const [globalFilter, setGlobalFilter] = useState<string | undefined>(undefined)
   const [selectedDate, setSelectedDate] = useState<Date | undefined>(new Date())
   const { technicianData, filteredTechnicianData } = useFetchTechnicians()
-
+  const [isLoading, setIsLoading] = useState(false)
   const [workOrderData, setWorkOrderData] = useState<BillsData[]>([
     {
       id: 0,
@@ -71,6 +73,7 @@ const Technicians = () => {
     color: 'white',
     backgroundColor: '#00426F',
     marginTop: '1rem',
+    border: '1px solid #00426F',
   }
 
   const TechnicianTableColumn = useMemo(
@@ -105,197 +108,198 @@ const Technicians = () => {
     setTechnicianRecord(e.data)
     setDataVisible(true)
   }
-  const workOrder = (
-    <>
-      <div className="flex gap-40 mt-3">
-        <div>
-          <p className="text-[18px] text-black w-[106px] h-[21px]"> Work Orders </p>
-        </div>
-        <div>
-          <SelectButton value={value} onChange={(e) => setValue(e.value)} options={options} />
-        </div>
-      </div>
-    </>
-  )
-  const actionButtons = [
-    () => (
-      <>
-        <div className="flex">
-          <Button
-            label="View"
-            style={{
-              fontWeight: 'bold',
-              textDecoration: 'underline',
-              cursor: 'pointer',
-            }}
-            // onClick={handleViewInventory}
-          />
-        </div>
-      </>
-    ),
-  ]
+
+  const ActionButtonColumn: ActionButtonColumnProps = {
+    header: '',
+    buttons: [
+      {
+        color: 'black',
+        label: 'View',
+        underline: true,
+        fontWeight: 500,
+        // onClick: (rowData) => handleEditButtonClick(rowData),
+      },
+    ],
+    headerStyle: {
+      fontSize: '10px',
+      height: '12px',
+      color: 'white',
+      backgroundColor: '#00426F',
+      marginTop: '1rem',
+      border: '1px solid #00426F',
+    },
+    style: { borderBottom: '1px solid #D5E1EA' },
+  }
 
   return (
     <>
       <Header header="MOORMANAGE/Technicians" />
-      <div className="flex justify-end ml-12">
-        <div className="flex gap-4 items-center mr-10 mt-14">
-          <div className="">
-            <p style={{ color: '#00426F' }}> Filter order by Date </p>
-          </div>
-          <div>
-            <Calendar
-              value={date}
-              onChange={(e) => setDate(e.value || null)}
-              selectionMode="range"
-              readOnlyInput
-            />
-          </div>
-        </div>
-      </div>
 
-      <div className="flex justify-around gap-10 mt-10">
-        <div
-          style={{
-            width: '700px',
-            height: '720px',
-            backgroundColor: '#FFFFFF',
-            border: '1px solid #D5E1EA',
-            borderRadius: '5px',
-            marginLeft: '3rem',
-          }}>
-          <InputTextWithHeader
-            placeholder={'search by name, ID....'}
-            inputTextStyle={{
-              height: '44px',
-              width: '100%',
-              cursor: 'pointer',
-              fontSize: '',
-              color: '#D5E1EA',
-              border: '1px solid #D5E1EA',
-              paddingLeft: '35px',
-              borderRadius: '5px',
-            }}
-            iconStyle={{
-              position: 'absolute',
-              left: '15px',
-              top: '50%',
-              transform: 'translateY(-50%)',
-              width: '18px',
-              height: '18px',
-            }}
-          />
-
-          <DataTableComponent
-            columns={TechnicianTableColumn}
-            scrollable={true}
-            tableStyle={{
-              border: '1px #D5E1EA',
-              backgroundColor: '#FFFFFF',
-            }}
-            data={technicianData}
-            emptyMessage={
-              <div className="text-center mt-40 mb-10">
-                <img
-                  src="/assets/images/empty.png"
-                  alt="Empty Data"
-                  className="w-20 mx-auto mb-4"
-                />
-                <p className="text-gray-500">No data available</p>
-              </div>
-            }
-          />
-        </div>
-
-        <div
-          // className={`${isLoading ? 'blur-screen' : ''}`}
-          style={{
-            flexGrow: 1,
-            borderRadius: '15px',
-            // border: '1px solid #D5E1EA',
-            backgroundColor: '#FFFFFF',
-            marginRight: '50px',
-            width: '700px',
-            height: '720px',
-            // border:"1px solid red"
-          }}>
-          <div className="flex ml-10 mt-6 gap-40 mb-3">
-            <div className="font-bold">
-              <p style={{ color: '#000000', fontSize: '18px' }}>{properties.workOrderHeader}</p>
+      <div className="mt-10">
+        <div className="flex justify-end mr-[54px]">
+          <div className="flex gap-4 items-center">
+            <div className="">
+              <p style={{ color: '#00426F' }}>Filter order by Date</p>
             </div>
-            <div>
-              <SelectButton value={value} onChange={(e) => setValue(e.value)} options={options} />
-            </div>
-          </div>
-
-          <InputTextWithHeader
-            // value={searchUsersText}
-            // onChange={handleUsersSearch}
-            placeholder="Search by name, ID, Email, Role, phone no..."
-            inputTextStyle={{
-              flexGrow: 1,
-              height: '44px',
-              border: '1px solid #C5D9E0',
-              padding: '0 2rem 0 2.5rem',
-              fontSize: '14px',
-              color: '#000000',
-              borderRadius: '4px',
-              minHeight: '44px',
-              fontWeight: 400,
-              backgroundColor: '#FFFFFF',
-            }}
-            iconStyle={{
-              position: 'absolute',
-              left: '15px',
-              top: '50%',
-              transform: 'translateY(-50%)',
-              width: '18px',
-              height: '18px',
-            }}
-          />
-          {/* {isLoading && (
-            <ProgressSpinner
+            <div
               style={{
-                position: 'absolute',
-                top: '50%',
-                transform: 'translate(-50%, -50%)',
-                width: '50px',
-                height: '50px',
-              }}
-              strokeWidth="4"
-            />
-          )} */}
+                position: 'relative',
+                border: '1px solid #D5E1EA',
+                borderRadius: '5px',
+                display: 'inline-block',
+              }}>
+              <Calendar
+                value={date}
+                onChange={(e) => setDate(e.value || null)}
+                selectionMode="range"
+                readOnlyInput
+                // placeholder="from: 15, March 2024   to: 21, March 2024"
+                className="h-8"
+                id="clander"
+              />
+              <img
+                src="/assets/images/Calendar.svg"
+                alt="Envelope Icon"
+                className="p-clickable"
+                style={{
+                  position: 'absolute',
+                  right: '10px',
+                  top: '50%',
+                  transform: 'translateY(-50%)',
+                  width: '20px',
+                  height: '15px',
+                }}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-around gap-10 mt-6 ">
           <div
             style={{
-              height: '600px',
-              overflow: 'auto',
-              padding: '0 20px',
-            }}
-            data-testid="customer-admin-users-table">
+              width: '700px',
+              height: '720px',
+              backgroundColor: '#FFFFFF',
+              border: '1px solid #D5E1EA',
+              borderRadius: '5px',
+              marginLeft: '3rem',
+            }}>
+            <InputTextWithHeader
+              placeholder="Search by name, ID..."
+              inputTextStyle={{
+                height: '44px',
+                width: '100%',
+                cursor: 'pointer',
+                fontSize: '',
+                color: '#D5E1EA',
+                border: '1px solid #D5E1EA',
+                paddingLeft: '40px',
+                borderRadius: '5px',
+              }}
+              iconStyle={{
+                position: 'absolute',
+                left: '15px',
+                top: '50%',
+                transform: 'translateY(-50%)',
+                width: '18px',
+                height: '18px',
+              }}
+            />
+
             <DataTableComponent
+              columns={TechnicianTableColumn}
+              scrollable={true}
               tableStyle={{
                 fontSize: '12px',
                 color: '#000000',
                 fontWeight: 600,
-                backgroundColor: '#D9D9D9',
+                backgroundColor: '#FFFFFF',
                 cursor: 'pointer',
               }}
-              scrollable={true}
-              columns={WorkOrdersColumn}
-              // scrollable={true}
-              data={workOrderData}
-              // actionButtons={ActionButtonUsersColumn}
+              data={technicianData}
               emptyMessage={
-                <div className="text-center mt-40">
+                <div className="text-center mt-40 mb-10">
                   <img
                     src="/assets/images/empty.png"
                     alt="Empty Data"
-                    className="w-32 mx-auto mb-4"
+                    className="w-20 mx-auto mb-4"
                   />
                   <p className="text-gray-500">No data available</p>
                 </div>
               }
             />
+          </div>
+
+          <div
+            className={`${isLoading ? 'blur-screen' : ''}`}
+            style={{
+              flexGrow: 1,
+              borderRadius: '5px',
+              border: '1px solid #D5E1EA',
+              backgroundColor: '#FFFFFF',
+              marginRight: '50px',
+              width: '700px',
+              height: '720px',
+              // border:"1px solid red"
+            }}>
+            <div className="flex justify-between mt-6  mb-3 ">
+              <div className="font-bold ml-5">
+                <p style={{ color: '#000000', fontSize: '18px' }}>{properties.workOrderHeader}</p>
+              </div>
+              <div className="mr-10">
+                <div className="card flex justify-content-center ">
+                  <SelectButton
+                    invalid
+                    value={value}
+                    onChange={(e: SelectButtonChangeEvent) => setValue(e.value)}
+                    options={options}
+                  />
+                </div>
+              </div>
+            </div>
+            {isLoading && (
+              <ProgressSpinner
+                style={{
+                  position: 'absolute',
+                  top: '50%',
+                  transform: 'translate(-50%, -50%)',
+                  width: '50px',
+                  height: '50px',
+                }}
+                strokeWidth="4"
+              />
+            )}
+            <div
+              style={{
+                overflow: 'auto',
+                // width:"100%"
+              }}
+              data-testid="customer-admin-users-table">
+              <DataTableComponent
+                tableStyle={{
+                  fontSize: '12px',
+                  color: '#000000',
+                  fontWeight: 600,
+                  backgroundColor: '#D9D9D9',
+                  cursor: 'pointer',
+                }}
+                scrollable={true}
+                columns={WorkOrdersColumn}
+                // scrollable={true}
+                data={[]}
+                actionButtons={ActionButtonColumn}
+                emptyMessage={
+                  <div className="text-center mt-40 mb-10">
+                    <img
+                      src="/assets/images/empty.png"
+                      alt="Empty Data"
+                      className="w-20 mx-auto mb-4"
+                    />
+                    <p className="text-gray-500">No data available</p>
+                  </div>
+                }
+              />
+            </div>
           </div>
         </div>
       </div>
