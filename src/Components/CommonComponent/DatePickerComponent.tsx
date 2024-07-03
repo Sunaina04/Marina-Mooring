4f81d7227c847a60fea10cab2f18b799d7eac1cc
Sunaina04 <sunaina.takhi@genboot.com>
2024-07-03 18:53:28 +0530

    IMPL getMooringDueForService , getAllOpenWorkOrder

--- src/Components/CommonComponent/DatePickerComponent.tsx
+++ src/Components/CommonComponent/DatePickerComponent.tsx
@@ -1,35 +1,33 @@
 import { useState } from 'react'
 // import { Calendar } from 'primereact/calendar'
-import { Calendar } from 'primereact/calendar';
+import { Calendar } from 'primereact/calendar'
 import { Nullable } from 'primereact/ts-helpers'
-import 'primereact/resources/themes/saga-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/saga-blue/theme.css'
+import 'primereact/resources/primereact.min.css'
+// import 'primeicons/primeicons.css';
 
 const DatePickerComponent = () => {
-  const [dates, setDates] = useState<Nullable<(Date | null)[]>>(null);
-  const [startDate, setStartDate] = useState(new Date());
-  
-  const [endDate, setEndDate] = useState(new Date());
+  const [dates, setDates] = useState<Nullable<(Date | null)[]>>(null)
+  const [startDate, setStartDate] = useState(new Date())
+
+  const [endDate, setEndDate] = useState(new Date())
 
   const handleDateChange = (e: { target: { value: any } }) => {
-    const { value } = e.target;
-   
-   // endDate.setDate(endDate.getDate() + 1);
+    const { value } = e.target
+
+    // endDate.setDate(endDate.getDate() + 1);
     if (value && value.length === 2 && value[0] && value[1]) {
-      setStartDate(value[0]);
-     
-      setEndDate(value[1]);
-      
+      setStartDate(value[0])
+
+      setEndDate(value[1])
     }
-  };
+  }
 
   return (
     <>
       <div
         className="card flex  justify-items-center"
         style={{
-        
           height: 'auto',
           gap: '0px',
           borderRadius: '10px',
@@ -39,13 +37,14 @@ const DatePickerComponent = () => {
           justifyContent: 'center',
           alignItems: 'center',
           marginBottom: '2rem',
-        
-        
         }}>
-           <Calendar value={dates} onChange={(e) => setDates(e.value)} selectionMode="range" 
-            hideOnRangeSelection inline/>
-
-        
+        <Calendar
+          value={dates}
+          onChange={(e) => setDates(e.value)}
+          selectionMode="range"
+          hideOnRangeSelection
+          inline
+        />
       </div>
     </>
   )
