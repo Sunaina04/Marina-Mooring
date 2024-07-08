2a87105ce57a86194cef900aab0600ca1b39cd8e
Sunaina04 <sunaina.takhi@genboot.com>
2024-07-08 17:30:05 +0530

    Staus for Mooring

--- src/Components/Map/CustomDisplayPositionMap.tsx
+++ src/Components/Map/CustomDisplayPositionMap.tsx
@@ -10,6 +10,23 @@ const CustomDisplayPositionMap: React.FC<CustomDisplayPositionMapProps> = ({
   zoomLevel,
   style,
 }) => {
+  const boxStyle = {
+    backgroundColor: 'white',
+    borderRadius: '10px',
+    padding: '10px',
+    marginTop: '10px',
+    boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
+  }
+
+  const dotStyle = (color: any) => ({
+    display: 'inline-block',
+    width: '10px',
+    height: '10px',
+    borderRadius: '50%',
+    backgroundColor: color,
+    marginRight: '10px',
+  })
+
   const [map, setMap] = useState<any>()
   const markerRef = useRef(null)
 
@@ -20,19 +37,35 @@ const CustomDisplayPositionMap: React.FC<CustomDisplayPositionMapProps> = ({
   }, [position, map])
 
   return (
-    <MapContainer
-      style={style}
-      center={position}
-      zoom={zoomLevel}
-      scrollWheelZoom={false}
-      attributionControl={false}
-      ref={setMap}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={position} ref={markerRef}></Marker>
-    </MapContainer>
+    <>
+      <MapContainer
+        style={style}
+        center={position}
+        zoom={zoomLevel}
+        scrollWheelZoom={false}
+        attributionControl={false}
+        ref={setMap}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position} ref={markerRef}></Marker>
+      </MapContainer>
+      <div style={boxStyle}>
+        <div>
+          <span style={dotStyle('red')}></span> Need Inspection
+        </div>
+        <div>
+          <span style={dotStyle('blue')}></span> Gear Off
+        </div>
+        <div>
+          <span style={dotStyle('green')}></span> Gear On
+        </div>
+        <div>
+          <span style={dotStyle('white')}></span> Not in Use
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/Components/Map/CustomMooringPositionMap.tsx
+++ src/Components/Map/CustomMooringPositionMap.tsx
@@ -28,6 +28,7 @@ const CustomMooringPositionMap: React.FC<CustomMooringPositionMapProps> = ({
   useEffect(() => {
     if (map && position) {
       map.setView(position)
+      map.x
     }
   }, [position, map])
 
@@ -36,6 +37,23 @@ const CustomMooringPositionMap: React.FC<CustomMooringPositionMapProps> = ({
     const [latitude, longitude] = coordinates.split(' ').map(parseFloat)
     return isNaN(latitude) || isNaN(longitude) ? null : [latitude, longitude]
   }
+
+  const boxStyle = {
+    backgroundColor: 'white',
+    borderRadius: '10px',
+    padding: '10px',
+    marginTop: '10px',
+    boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
+  }
+
+  const dotStyle = (color: any) => ({
+    display: 'inline-block',
+    width: '10px',
+    height: '10px',
+    borderRadius: '50%',
+    backgroundColor: color,
+    marginRight: '10px',
+  })
 
   const iconsByStatusId = {
     1: GearOnIcon,
@@ -79,6 +97,20 @@ const CustomMooringPositionMap: React.FC<CustomMooringPositionMapProps> = ({
             )
           })}
       </MapContainer>
+      <div style={boxStyle}>
+        <div>
+          <span style={dotStyle('red')}></span> Need Inspection
+        </div>
+        <div>
+          <span style={dotStyle('blue')}></span> Gear Off
+        </div>
+        <div>
+          <span style={dotStyle('green')}></span> Gear On
+        </div>
+        <div>
+          <span style={dotStyle('white')}></span> Not in Use
+        </div>
+      </div>
     </>
   )
 }
