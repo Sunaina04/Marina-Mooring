6ab9f31dd32fbc7335a57123289a4f12afea8197
ArunGenboot <arun.kumar@genboot.com>
2024-07-12 18:29:31 +0530

    fixed button border in workOrder

--- src/Components/Moorpay/AccountReceivable/AccountRecievable.tsx
+++ src/Components/Moorpay/AccountReceivable/AccountRecievable.tsx
@@ -38,7 +38,7 @@ const AccountRecievable = () => {
   const [totalRecords, setTotalRecords] = useState<number>()
   const [totalRecordsInvoice, setTotalRecordsInvoice] = useState<number>()
   const [isLoading, setIsLoading] = useState(false)
-  const [denyModalOpen, setDenyModalOpen] = useState(false)
+  const [denyModalOpen, setDenyModalOpen] = useState(true)
   const [isPaymentModalOpen, setIsPaymentModalOpen] = useState(false)
   const [isContactModalOpen, setIsContactModalOpen] = useState(false)
   const [addWorkOrderModal, setAddWorkOrderModal] = useState(false)
@@ -689,10 +689,10 @@ const AccountRecievable = () => {
       <Dialog
         position="center"
         style={{
-          width: '800px',
-          minWidth: '800px',
-          height: '480px',
-          minHeight: '480px',
+          width: '520px',
+          minWidth: '520px',
+          height: '420px',
+          minHeight: '420px',
           borderRadius: '1rem',
           fontWeight: '400',
           cursor: 'alias',
@@ -702,7 +702,7 @@ const AccountRecievable = () => {
         visible={denyModalOpen}
         onHide={handleModalClose}
         header="Deny">
-        <ReasonModal selectedRowData={undefined} setVisible={() => {}} closeModal={() => {}} />
+        <ReasonModal selectedRowData={undefined} setVisible={() => { }} closeModal={() => { }} />
       </Dialog>
 
       {/* for view button */}
@@ -725,8 +725,8 @@ const AccountRecievable = () => {
         <AddWorkOrders
           workOrderData={undefined}
           isAccountRecievable={true}
-          setVisible={() => {}}
-          closeModal={() => {}}
+          setVisible={() => { }}
+          closeModal={() => { }}
         />
       </Dialog>
     </>
