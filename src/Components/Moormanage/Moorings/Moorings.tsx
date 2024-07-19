0c45c3e1963434228f71df28255509ca0d6d7494
harsh <harsh.devgan@genboot.com>
2024-07-19 11:18:36 +0530

    fixed color issue on mooring page and fixed amount issue on approve modal

--- src/Components/Moormanage/Moorings/Moorings.tsx
+++ src/Components/Moormanage/Moorings/Moorings.tsx
@@ -660,7 +660,7 @@ const Moorings = () => {
               }}
               className="ml-[45px] w-[20px] flex-1">
               <div data-testid="customer-data" className="flex flex-col h-full ">
-                <div className="flex item-center justify-between bg-[#10293A]  rounded-tl-[10px] rounded-tr-[10px] text-white cursor-pointer">
+                <div className="flex item-center justify-between bg-[#00426F]  rounded-tl-[10px] rounded-tr-[10px] text-white cursor-pointer">
                   <div>
                     <h1 className="p-4 text-xl font-extrabold">{properties.mooringHeader}</h1>
                   </div>
@@ -783,7 +783,7 @@ const Moorings = () => {
               minHeight: '700px',
               width: '40px',
               minWidth: '40px',
-              backgroundColor: '#10293A',
+              backgroundColor: '#00426F',
             }}
             className="rounded-md ml-[45px]">
             <div
@@ -842,7 +842,7 @@ const Moorings = () => {
                 width: '450px',
               }}
               className="flex-grow border bg-white">
-              <div className="bg-[#10293A]  rounded-t-[10px] flex justify-between">
+              <div className="bg-[#00426F]  rounded-t-[10px] flex justify-between">
                 <div className="text-sm font-semibold rounded-t-md">
                   <h1 className="p-3 text-white text-lg font-extrabold">
                     {properties.customerRecord}
@@ -924,7 +924,7 @@ const Moorings = () => {
                   marginBottom: '0px',
                 }}>
                 <label
-                  style={{ backgroundColor: '#10293A' }}
+                  style={{ backgroundColor: '#00426F' }}
                   htmlFor="faq1"
                   className="cursor-pointer flex items-center justify-between h-14"
                   onClick={() => handleToggle('faq1')}>
@@ -1119,7 +1119,7 @@ const Moorings = () => {
               minHeight: '700px',
               width: '40px',
               minWidth: '40px',
-              backgroundColor: '#10293A',
+              backgroundColor: '#00426F',
             }}
             className="rounded-md ml-[20px] mr-[20px]">
             <div className="p-3" onClick={() => setIsMooringExpanded(true)} style={{ cursor: 'pointer' }}>
