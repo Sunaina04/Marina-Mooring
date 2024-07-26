5b6b77f5df38e4a3c66ac14d4c51b4b6bbffbb09
Sunaina04 <sunaina.takhi@genboot.com>
2024-07-26 17:20:07 +0530

    CustomerOwner name issue fixed

--- src/Components/Layout/LayoutComponents/Header.tsx
+++ src/Components/Layout/LayoutComponents/Header.tsx
@@ -22,9 +22,8 @@ const Header: React.FC<HeaderProps> = ({ header, customer }) => {
   }
 
   const handleCustomerIdSelection = (customerId: any) => {
-    console.log(customerId, 'customerID')
-
-    dispatch(setCustomerName(customerId?.name))
+    const firstLastName = customerId?.firstName + ' ' + customerId?.lastName
+    dispatch(setCustomerName(firstLastName))
     dispatch(setCustomerId(customerId?.id))
   }
 
@@ -87,8 +86,6 @@ const Header: React.FC<HeaderProps> = ({ header, customer }) => {
           <Dropdown
             value={selectedCustomerName}
             onChange={(e) => {
-              console.log('e', e)
-
               handleCustomerIdSelection(e.value)
             }}
             optionLabel="label"
