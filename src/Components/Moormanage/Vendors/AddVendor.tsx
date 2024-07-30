9657b8d8a5aa165370c41a831370cab6215510d0
Sunaina04 <sunaina.takhi@genboot.com>
2024-07-30 12:17:41 +0530

    Vendor Edit Issue fixed

--- src/Components/Moormanage/Vendors/AddVendor.tsx
+++ src/Components/Moormanage/Vendors/AddVendor.tsx
@@ -24,7 +24,7 @@ const AddVendor: React.FC<AddVendorProps> = ({ vendors, editMode, closeModal, ge
     phone: '',
     website: '',
     countryForAddress: '',
-    stateForAddress: '', 
+    stateForAddress: '',
     zipCodeForAddress: '',
     emailForAddress: '',
     countryForRemit: '',
@@ -88,10 +88,10 @@ const AddVendor: React.FC<AddVendorProps> = ({ vendors, editMode, closeModal, ge
       setIsLoading(false)
       setStatesData(statesData)
     } else {
-      setFormData({
-        ...formData,
-        ['stateForAddress']: '',
-      })
+      setFormData((prevFormData: any) => ({
+        ...prevFormData,
+        stateForAddress: '',
+      }))
     }
   }, [formData?.countryForAddress?.id])
 
