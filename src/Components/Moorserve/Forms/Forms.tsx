d26560b4aa2a286d211220ea7a60a64049f8b7ce
Sunaina04 <sunaina.takhi@genboot.com>
2024-07-31 11:57:23 +0530

    FileName issue sorted in Form Section

--- src/Components/Moorserve/Forms/Forms.tsx
+++ src/Components/Moorserve/Forms/Forms.tsx
@@ -74,6 +74,7 @@ const Forms = () => {
       const dummyUrl = convertBytetoUrl(rowData.formData)
       const link = document.createElement('a')
       link.href = dummyUrl
+
       const name = rowData.fileName
       link.setAttribute('download', name) //or any other extension
       document.body.appendChild(link)
