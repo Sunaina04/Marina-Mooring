2e8f314fcd680e215ade160f6058587587b08ed1
Sunaina04 <sunaina.takhi@genboot.com>
2024-09-05 15:19:35 +0530

    DOMException issue fixed

--- src/Store/Store.ts
+++ src/Store/Store.ts
@@ -24,6 +24,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: 'root',
   storage,
+  whitelist: ['user'],
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
